2fb75b294b60aeed693d74115835cfd96a34e725
sabmeua <sabme.ua@gmail.com>
2020-04-22 17:32:49 +0900

    Fix wording

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello world. ver.1!'),
+        body: JSON.stringify('こんにちは 世界. ver.1!'),
         headers: {
             'Access-Control-Allow-Origin': '*',
         },
